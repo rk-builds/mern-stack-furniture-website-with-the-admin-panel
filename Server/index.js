998acdbd61469e780624b070ee8086ce559727f3
rk-builds <Kothariruchi95@gmail.com>
2026-02-07 01:11:30 +0530

    Refactor server setup and improve admin model handling
    Refactor imports and update admin model creation method. Adjust database connection handling and server start logging.changes due to vercel have error in app.listen()

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,22 @@
-let express = require("express")
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
 require("dotenv").config()
-let app = express()
-let cors = require("cors")
-let mongoose = require("mongoose")
+
 const { adminRoutes } = require("./App/Routes/admin/adminRoutes")
 const { adminAuthModel } = require("./App/models/adminAuthModel")
 const { webRoutes } = require("./App/Routes/website/webRouts")
 
+const app = express()
+
 app.use(cors())
 app.use(express.json())
 
-//web:
-app.use("/web",webRoutes)
-//admin:
-
+// routes
+app.use("/web", webRoutes)
 app.use("/admin", adminRoutes)
 
+// static folders
 app.use('/uploads/category', express.static('uploads/category'))
 app.use('/uploads/subCategory', express.static('uploads/subCategory'))
 app.use('/uploads/subSubCategory', express.static('uploads/subSubCategory'))
@@ -24,21 +25,22 @@ app.use('/uploads/whyChoose', express.static('uploads/whyChoose'))
 app.use('/uploads/test', express.static('uploads/test'))
 app.use('/uploads/slider', express.static('uploads/slider'))
 app.use('/uploads/adminprofile', express.static('uploads/adminProfile'))
+
+// DB connect (NO listen here)
 mongoose.connect(process.env.DBCONECTIONURL)
-  .then(async (res) => {
-
-    app.listen(process.env.PORT)
-
+  .then(async () => {
     let data = await adminAuthModel.find()
-    if (data.length == 0) {
-      await adminAuthModel.insertOne(
-        {
-          name: 'admin',
-          email: 'kothariruchi95@gmail.com',
-          password: 'admin@123',
-          phone: '8888444555'
-        }
-      )
+    if (data.length === 0) {
+      await adminAuthModel.create({
+        name: 'admin',
+        email: 'kothariruchi95@gmail.com',
+        password: 'admin@123',
+        phone: '8888444555'
+      })
     }
-    console.log("server start")
+    console.log("DB connected")
   })
+  .catch(err => console.log("DB error", err))
+
+//  THIS IS THE KEY LINE FOR VERCEL
+module.exports = app
