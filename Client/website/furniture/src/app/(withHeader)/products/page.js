89a8ee4a8c444511dcf55a213a1f53a6bc4adade
rk-builds <Kothariruchi95@gmail.com>
2026-02-10 21:58:51 +0530

    Refactor Product component and clean up imports

--- Client/website/furniture/src/app/(withHeader)/products/page.js
+++ Client/website/furniture/src/app/(withHeader)/products/page.js
@@ -1,8 +1,4 @@
-import { fetchParents, getProducts } from '@/app/apiServices/productServices'
-import React from 'react'
-// import ProductCat from './ProductCat'
-// import ProductListing from './ProductListing'
-// import { FaProductHunt } from 'react-icons/fa6'
+import { fetchParents } from '@/app/apiServices/productServices'
 import Breadcrumb from '@/app/common/Breadcrumb'
 import ProductWrapper from './ProductWrapper'
 
@@ -11,24 +7,17 @@ export const metadata = {
   description: "About our products",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function Product() {
+  let parent = await fetchParents();
 
-
-  let parent= await fetchParents()
-  console.log(parent)
-
-  let pageName="Products"
   return (
     <>
-      <Breadcrumb pageName={pageName}/>
-
-         <div className='max-w-[1320px} mt-[50px] mx-auto'>
-            {/* <ProductCat product={parent}/> */}
-            <ProductWrapper parents={parent} />
-            {/* <ProductListing /> */}
-         </div>
-    
-
+      <Breadcrumb pageName="Products" />
+      <div className="max-w-[1320px] mt-[50px] mx-auto">
+        <ProductWrapper parents={parent} />
+      </div>
     </>
-  )
+  );
 }
